2a7d550a3a2dd5b4b8263262e41f60235fe3986b
Zachary Doherty <zacharyedoherty@gmail.com>
2024-01-30 16:00:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+  it('is declared as equal to Chelsea', function () {
+    expect(mostProfitableNeighborhood).to.equal('Chelsea');
+});
+
+let companyCeo = "Susan Smith";
+  it('is declared as equal to Susan Smith', function () {
+    expect(companyCeo).to.equal('Susan Smith');
+});
+
